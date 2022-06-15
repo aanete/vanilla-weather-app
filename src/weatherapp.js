ae25c6580db809b9a369c7ae5270c4a937316f03
aanete <alksne.anete@gmail.com>
2022-06-16 01:05:48 +0200

    Query listeners added; stuck on date and time

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -1,8 +1,19 @@
+
 function displayTemperature(response) {
-    console.log(response.data.main.temp);
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = (response.data.name);
+    let skyElement = document.querySelector("#sky");
+    skyElement.innerHTML = (response.data.weather[0].description);
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = (response.data.main.humidity);
+   
 }
+
 
 let apiKey = "f6ee4e21ef3e79eaac68ef17a64ce0d6";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
