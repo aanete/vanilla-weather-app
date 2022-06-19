6095888f7a6dd44002ae8e227cf4c5332f930826
aanete <alksne.anete@gmail.com>
2022-06-19 20:18:03 +0200

    Search form and action

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -44,10 +44,20 @@ function displayTemperature(response) {
  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
+function search(city) {
 let apiKey = "f6ee4e21ef3e79eaac68ef17a64ce0d6";
-
-let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    console.log(cityInputElement.value);
+}
+
+search("Paris");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
