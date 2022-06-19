c9f912e38429e4845d255433b3960f98b671eb7d
aanete <alksne.anete@gmail.com>
2022-06-19 19:11:03 +0200

    Time_upscale

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -12,7 +12,13 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
     date.getDay();
     let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;
+    }
     let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
 return `${day}, ${hours}:${minutes}`;
 }
 
